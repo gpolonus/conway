4cba5178cd6a247756974fce374f8ad14870d998
polonusg <polonusg@hnicorp.com>
2020-02-10 13:03:40 -0600

    Removed some functionality of the app

--- src/containers/GameControls/GameControls.js
+++ src/containers/GameControls/GameControls.js
@@ -6,12 +6,12 @@ export default ({toggleRun, running, step, save, restore, clearGrid, getOutput, 
     <div>
       <button onClick={toggleRun}>{running ? 'Stop' : 'Run'}</button>
       <button onClick={step}>Step</button>
-      <button onClick={save}>Save</button>
-      <button onClick={restore} disabled={saved === ''}>Restore</button>
+      {/* <button onClick={save}>Save</button> */}
+      {/* <button onClick={restore} disabled={saved === ''}>Restore</button> */}
       <button onClick={random}>Random</button>
       <button onClick={clearGrid}>Clear</button>
-      <div style={{padding: '10px 0'}}><textarea value={getOutput()} onChange={event => setOutput(event.target.value)}></textarea></div>
-      <div>{getOutput().length}</div>
+      {/* <div style={{padding: '10px 0'}}><textarea value={getOutput()} onChange={event => setOutput(event.target.value)}></textarea></div> */}
+      {/* <div>{getOutput().length}</div> */}
     </div>
   );
 }
